f244bae3c365c868098ef0ab6f40d54966f43725
NORIKIM <jinagold.k@gmail.com>
2023-12-10 00:03:10 +0900

    프로그래머스-연속된 수의 합

---
+++ algorithms/Programmers/연속된 수의 합.swift
@@ -0,0 +1,37 @@
+//
+//  main.swift
+//  algorithms
+//
+//  Created by 김지나 on 2023/12/09.
+//
+
+import Foundation
+
+func solution(_ num:Int, _ total:Int) -> [Int] {
+    var result = [Int]()
+    var baseNumber = num
+    var number = num
+    
+    while true {
+        for _ in 0 ..< num {
+            result.append(number)
+            number += 1
+        }
+        
+        let resultSum = result.reduce(0, +)
+        if resultSum == total {
+            break
+        } else if resultSum > total {
+            result.removeAll()
+            baseNumber -= 1
+            number = baseNumber
+        } else {
+            result.removeAll()
+            baseNumber += 1
+            number = baseNumber
+        }
+    }
+    
+    return result
+}
+
