e4cc2c3a9680f8e1f8c0f48808ad1fe5f16613d1
NORIKIM <jinagold.k@gmail.com>
2023-12-11 21:43:06 +0900

    프로그래머스-배열만들기2

---
+++ algorithms/Programmers/배열만들기2.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  algorithms
+//
+//  Created by 김지나 on 2023/12/11.
+//
+
+import Foundation
+
+func solution(_ l:Int, _ r:Int) -> [Int] {
+    var result = [Int]()
+    
+    for i in l ... r {
+        if i % 5 == 0 {
+            var iString = String(i).replacingOccurrences(of: "0", with: "")
+            iString = iString.replacingOccurrences(of: "5", with: "")
+            if iString == "" {
+                result.append(i)
+            }
+        }
+    }
+    
+    return result.isEmpty ? [-1] : result
+}
