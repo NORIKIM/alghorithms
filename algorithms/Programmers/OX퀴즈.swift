a57ffbfb7b0ccb0084d7503f50b9cb55c572cb08
NORIKIM <jinagold.k@gmail.com>
2023-12-14 18:34:39 +0900

    프로그래머스-OX퀴드 파일 오류 수정

--- algorithms/Programmers/OX퀴즈.swift
+++ algorithms/Programmers/OX퀴즈.swift
@@ -1,29 +1,29 @@
 //
-//  main.swift
+//  OX퀴즈.swift
 //  algorithms
 //
-//  Created by 김지나 on 2023/12/13.
+//  Created by 김지나 on 2023/12/14.
 //
 
 import Foundation
 
 func solution(_ quiz:[String]) -> [String] {
     var result = [String]()
-    
+
     for i in quiz {
         let function = i.split(separator: " ")
         let operate = function[1]
         var calculate = 0
-        
+
         if operate == "+" {
             calculate = Int(function[0])! + Int(function[2])!
         } else {
             calculate = Int(function[0])! - Int(function[2])!
         }
-        
+
         let isEqual = calculate == Int(function[4]) ? "O" : "X"
         result.append(isEqual)
     }
-    
+
     return result
 }
