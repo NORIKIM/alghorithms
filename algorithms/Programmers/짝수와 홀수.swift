1a954d1ea3ce9bd9d2f3022489f725381689fbd6
NORIKIM <jinagold.k@gmail.com>
2024-01-10 19:34:01 +0900

    프로그래머스-짝수와홀수

---
+++ algorithms/Programmers/짝수와 홀수.swift
@@ -0,0 +1,12 @@
+//
+//  짝수와 홀수.swift
+//  algorithms
+//
+//  Created by 김지나 on 2024/01/10.
+//
+
+import Foundation
+
+func solution(_ num:Int) -> String {
+    return num % 2 == 0 ? "Even" : "Odd"
+}
